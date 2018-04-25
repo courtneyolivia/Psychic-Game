9fd11eef067c18f40ad6e5babff35d07c532935a
sakiohma <sakiohma@github.com>
2018-04-25 03:44:45 -0400

    added changes

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,42 +1,92 @@
-document.write("Guess what letter I'm thinking of <br/><br/>");
-document.write("Wins:<br/><br/>");
-document.write("Losses:<br/><br/>");
-document.write("Guesses Left:<br/><br/>");
-document.write("Your Guesses So Far:<br/><br/>");
+
+alert("Ladies and gentlemen!...");
+alert("Come one, come all. Now featuring, the one and only...");
+alert("The amazing, the spectacular...");
+alert("MADAME ESMERALDA!!!!!!!");
 
 
+// We're creating a list of three options for the letters
+    var letters = ["a", "b", "c"];
+
+    // This array will hold what we guess
+    var guessedLetters = [];
+
+    // This variable will be randomly assigned one of the three letters
+    var letterToGuess = null;
+
+    // This is what we'll use to count down
+    var guessesLeft = 9;
+
+    // This is the counter for wins/losses
+    var wins = 0;
+    var losses = 0;
+
+    // Below we created three functions to updateGuesses, updateGuessesLeft, and updateGuessesSoFar
+    var updateGuessesLeft = function() {
+      // Here we are grabbing the HTML element and setting it equal to the guessesLeft.
+      // (i.e. guessesLeft will get displayed in HTML)
+      document.querySelector("#guesses-left").innerHTML = guessesLeft;
+    };
+
+    var updateLetterToGuess = function() {
+      // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)
+      letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+    };
+
+    var updateGuessesSoFar = function() {
+      // Here we take the guesses the user has tried -- then display it as letters separated by commas.
+      document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
+    };
+
+    // Function will be called when we reset everything
+    var reset = function() {
+      guessesLeft = 9;
+      guessedLetters = [];
+      updateLetterToGuess();
+      updateGuessesLeft();
+      updateGuessesSoFar();
+    };
+
+    // Execute on page load.
+    updateLetterToGuess();
+    updateGuessesLeft();
+
+    // This function will capture the keyboard clicks.
+    document.onkeyup = function(event) {
+      // It's going to reduce the guesses by one
+      guessesLeft--;
+
+      // Lowercase the letter
+      var letter = String.fromCharCode(event.keyCode).toLowerCase();
+
+      // Then add the guess to the guessedLetters array
+      guessedLetters.push(letter);
+
+      // Then its going to run the update functions
+      updateGuessesLeft();
+      updateGuessesSoFar();
 
 
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
-'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+      // We'll check if there's a match.
+      if (letter === letterToGuess) {
 
-// Variables for tracking our wins, losses and ties. They begin at 0.
-var wins = 0;
-var losses = 0;
-var guesses = 10;
+        // If there is then we win and we'll update the HTML to display the win.
+        wins++;
+        document.querySelector("#wins").innerHTML = wins;
 
-var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-console.log(computerGuess)
-
+        // Then we'll reset the game
+        reset();
+      }
 
 
-// When the user presses a key, it will run the following function...
-document.onkeypress = function(event) {
-var userGuess = event.key;
+      // If we are out of guesses...
+      if (guessesLeft === 0) {
 
-if(userGuess === computerGuess){
-    wins++;
-}else{
-    guesses--;
-}
+        // Then we will loss and we'll update the HTML to display the loss.
+        losses++;
+        document.querySelector("#losses").innerHTML = losses;
 
-if(guesses = 0){
-    losses++
-}
-  
-
-document.getElementById('wins').innerHTML = "Wins: " + wins;
-document.getElementById('losses').innerHTML = "losses: " + losses;
-document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
-}
+        // Then we'll call the reset.
+        reset();
+      }
+    };
